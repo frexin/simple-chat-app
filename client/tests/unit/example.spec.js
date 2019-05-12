4a232c696d4ef6f35cb88c81b49a5bea85ba7f51
Senkevich Kirill <sks89@mail.ru>
2019-05-12 14:29:04 +0300

    very simple unit test

--- client/tests/unit/example.spec.js
+++ client/tests/unit/example.spec.js
@@ -1,12 +1,10 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import {mount, shallowMount} from '@vue/test-utils'
+import App from '@/App.vue'
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+    const wrapper = mount(App);
+
+    expect(wrapper.contains('div.msg-history')).toBe(true);
   })
-})
+});
